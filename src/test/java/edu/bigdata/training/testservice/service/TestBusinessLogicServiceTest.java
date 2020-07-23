05e63615dab8320a4162e3a443270c2df19aeaef
ruakete <1@1.ru>
2020-07-23 22:25:51 +0200

    IRM-TEST-072020-1

---
+++ src/test/java/edu/bigdata/training/testservice/service/TestBusinessLogicServiceTest.java
@@ -0,0 +1,83 @@
+package edu.bigdata.training.testservice.service;
+
+import edu.bigdata.training.testservice.controller.model.Person;
+import edu.bigdata.training.testservice.dao.TestServiceRepository;
+import edu.bigdata.training.testservice.model.PersonEntity;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {TestBusinessLogicServiceTest.TestBusinessLogicServiceTestConfiguration.class})
+public class TestBusinessLogicServiceTest {
+
+    @Autowired
+    private TestBusinessLogicService testBusinessLogicService;
+
+    @Autowired
+    private TestServiceRepository testServiceRepository;
+
+    @Test
+    public void testCreate(){
+        Person person = new Person("test");
+
+        PersonEntity personEntity = testBusinessLogicService.processCreate(person);
+
+        Assert.assertEquals(person.getName(), personEntity.getName());
+        Mockito.verify(testServiceRepository, Mockito.times(1)).save(personEntity);
+
+    }
+
+    @Test
+    public void testGet(){
+        PersonEntity personEntityList = testBusinessLogicService.processGet(UUID.randomUUID().toString());
+
+        Assert.assertEquals("name", personEntityList.getName());
+        Mockito.verify(testServiceRepository, Mockito.times(1)).get(any());
+
+    }
+
+    @Test
+    public void testGetALL(){
+        List<PersonEntity> personEntityList = testBusinessLogicService.processGetAll();
+
+        Assert.assertEquals("name1", personEntityList.get(0).getName());
+        Assert.assertEquals("name2", personEntityList.get(1).getName());
+        Mockito.verify(testServiceRepository, Mockito.times(1)).getAll();
+
+    }
+
+    @Configuration
+    static class TestBusinessLogicServiceTestConfiguration {
+
+        @Bean
+        TestServiceRepository testServiceRepository() {
+            TestServiceRepository testServiceRepository = mock(TestServiceRepository.class);
+            when(testServiceRepository.get(any())).thenReturn(new PersonEntity("name"));
+            when(testServiceRepository.getAll())
+                    .thenReturn(Arrays.asList(new PersonEntity("name1"),new PersonEntity("name2")));
+            return testServiceRepository;
+        }
+
+        @Bean
+        TestBusinessLogicService testBusinessLogicService(TestServiceRepository testServiceRepository){
+            return new TestBusinessLogicService(testServiceRepository);
+        }
+    }
+
+}
