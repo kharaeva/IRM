16cd9186d9b3dd6abe9776f5a67cf6626a93c2ea
ruakete <1@1.ru>
2020-09-25 19:17:51 +0200

    IRM-TEST-072020-1

--- build.sh
+++ build.sh
@@ -9,7 +9,8 @@ docker login -u="$2" -p="$3"
 docker push "$1"
 
 kubectl apply -f test-service-configmap.yaml
-cd src/main/resources/keystore/local/
+cd ./src/main/resources/keystore/local/
+pwd
 kubectl create secret generic testservicesecret --from-literal=keystore_password=1234567890 --from-file=keystore.p12=keystore.p12 --from-literal=truststore_password=1234567890 --from-file=truststore.jks=truststore.jks
 
 cd ../../../../..
